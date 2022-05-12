3f0183a5fd7765744f91b7208cc8f97b8ae49157
David Yan <david.yan.software@gmail.com>
2022-05-11 20:30:28 -0700

    implimenting cloudscript battle

--- Assets/Scripts/Leaderboard/Opponent.cs
+++ Assets/Scripts/Leaderboard/Opponent.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using PlayFab;
+using PlayFab.CloudScriptModels;
 
 public class Opponent : MonoBehaviour
 {
@@ -28,19 +30,36 @@ public class Opponent : MonoBehaviour
 
     public void OnClick()
     {
-        Battle(player.PlayfabId, this.playfabId);
+        CallTestBattle(this.playfabId);
     }
 
-    private void Battle(string playerId, string opponentId)
+    private void CallTestBattle(string playfabId)
     {
-        Debug.Log("PlayerId: " + playerId + "\nOpponentId: " + opponentId);
-
-        // fetch stats of both players
-
-        // simulate battle
-
-        // increment decrement rating of both
-
-        // return result - rating change?
+        ExecuteFunctionRequest request = new ExecuteFunctionRequest()
+        {
+            Entity = new EntityKey()
+            {
+                Id = PlayFabSettings.staticPlayer.EntityId, //Get this from when you logged in,
+                Type = PlayFabSettings.staticPlayer.EntityType, //Get this from when you logged in
+            },
+            FunctionName = "TestBattle", //This should be the name of your Azure Function that you created.
+            FunctionParameter = new Dictionary<string, object>() { { "opponent", playfabId } }, //This is the data that you would want to pass into your function.
+            GeneratePlayStreamEvent = false //Set this to true if you would like this call to show up in PlayStream
+        };
+        PlayFabCloudScriptAPI.ExecuteFunction(
+            request,
+            (ExecuteFunctionResult result) => {
+                if (result.FunctionResultTooLarge ?? false)
+                {
+                    Debug.Log("This can happen if you exceed the limit that can be returned from an Azure Function, See PlayFab Limits Page for details.");
+                    return;
+                }
+                Debug.Log($"The {result.FunctionName} function took {result.ExecutionTimeMilliseconds} to complete");
+                Debug.Log($"Result: {result.FunctionResult.ToString()}");
+            },
+            (PlayFabError error) => {
+                Debug.Log($"Opps Something went wrong: {error.GenerateErrorReport()}");
+            }
+        );
     }
 }
